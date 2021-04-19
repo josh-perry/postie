995689da5b0e386549602fdeddbdddf917b6fc94
Josh Perry <joshperry@outlook.com>
2021-04-20 00:25:03 +0100

    Delete key goes brr

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,28 +1,9 @@
 <template>
   <div>
-    <hero />
-    <hr />
-    <home-content />
+
   </div>
 </template>
 
 <script>
-import Hero from "../components/Hero";
-import HomeContent from "../components/HomeContent";
-
-export default {
-  name: "home",
-  components: {
-    Hero,
-    HomeContent
-  }
-};
+export default {}
 </script>
-
-<style lang="scss" scoped>
-.next-steps {
-  .fa-link {
-    margin-right: 5px;
-  }
-}
-</style>

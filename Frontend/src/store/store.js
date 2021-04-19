59e9de757c31ca3cb84daabe732a4a4785cb6843
Josh Perry <joshperry@outlook.com>
2021-04-20 00:24:28 +0100

    I don't know what I'm doing

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -5,6 +5,18 @@ Vue.use(Vuex)
 
 import { getInstance } from "@/auth";
 
+function getToken(context, instance, resolve, reject) {
+  instance
+    .getTokenSilently()
+    .then(authToken => {
+      context.commit("setToken", authToken);
+      resolve(authToken);
+    })
+    .catch(error => {
+      reject(error);
+    });
+}
+
 export const store = new Vuex.Store({
   state: {
     token: null
@@ -18,17 +30,19 @@ export const store = new Vuex.Store({
     retrieveTokenFromAuth0(context) {
       return new Promise((resolve, reject) => {
         const instance = getInstance();
+
+        if (instance.loading === false) {
+          if (instance.isAuthenticated) {
+            getToken(context, instance, resolve, reject)
+          }
+          else {
+            resolve(null)
+          }
+        }
+
         instance.$watch("loading", loading => {
           if (loading === false && instance.isAuthenticated) {
-            instance
-              .getTokenSilently()
-              .then(authToken => {
-                context.commit("setToken", authToken);
-                resolve(authToken);
-              })
-              .catch(error => {
-                reject(error);
-              });
+            getToken(context, instance, resolve, reject);
           }
           else if (loading === false) {
             resolve(null);

a00d5872bb30075e3a8743dd1523cf5edd5c1f97
Josh Perry <joshperry@outlook.com>
2021-06-30 20:37:14 +0100

    Added sidebar placeholder

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,10 +2,17 @@
   <div>
     <NavBar />
 
+    <Error />
+
     <div class="container">
-      <Error />
-      <div>
-        <RouterView />
+      <div class="main">
+        <div class="route-wrapper">
+          <RouterView />
+        </div>
+      </div>
+
+      <div class="sidebar">
+        <SideBar />
       </div>
     </div>
   </div>
@@ -13,11 +20,13 @@
 
 <script>
 import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
 import Error from "./components/Error";
 
 export default {
   components: {
     NavBar,
+    SideBar,
     Error
   }
 };
@@ -27,9 +36,21 @@ export default {
 .container {
   margin: 2em auto;
   margin-top: 0;
-  max-width: 800px;
   padding: 1em;
   line-height: 1.4;
-  text-align: justify;
+  display: inline-flex;
+}
+
+.main {
+  flex: 4;
+}
+
+.route-wrapper {
+  max-width: 880px;
+  margin: auto;
+}
+
+.sidebar {
+  flex: 1;
 }
 </style>
---
+++ Frontend/src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Header</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio sem. Donec fringilla accumsan justo, ut volutpat augue. Nunc suscipit massa ac tristique laoreet. Duis eget placerat dui. Suspendisse dapibus dolor ac velit sodales sodales. Etiam viverra, nunc non vulputate scelerisque, quam mi sodales lectus, et ultrices turpis leo ut massa. Etiam a feugiat ex. Nullam posuere a velit quis sagittis. </p>
+
+    <p>Maecenas et neque efficitur, congue diam ut, rutrum nisl. Nam viverra metus nec nulla euismod, non luctus arcu feugiat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sed commodo augue, id dapibus lacus. Nullam at erat mollis, ultricies enim eu, suscipit enim. Nam tempus nibh eget bibendum semper. Aliquam sollicitudin viverra diam, viverra fringilla sem dapibus non. Donec ut erat ut urna finibus ornare id quis elit. Suspendisse nec ipsum id diam sodales vestibulum convallis a eros. Nulla at eros ultrices lacus tempus sodales ut non leo. Suspendisse scelerisque nunc id mi viverra ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec venenatis dapibus porta. Maecenas pulvinar pulvinar massa, vitae eleifend ipsum elementum at. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+
+    <h4>Subheader</h4>
+    <p> Duis ornare purus dolor. Proin in dictum odio. Fusce congue odio eu suscipit pellentesque. Sed urna mi, varius non semper nec, molestie non massa. Quisque a est ac ante sagittis auctor. Ut fermentum sem sed convallis fermentum. Curabitur nec ipsum erat. Curabitur id metus at magna molestie pretium. Donec mollis suscipit lectus, nec lacinia magna venenatis et.</p>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>

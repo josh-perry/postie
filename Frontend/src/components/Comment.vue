2b0ff18f8c7affe72fa8f8255eea7d923ff29d32
Josh Perry <joshperry@outlook.com>
2021-07-04 19:55:36 +0100

    Nicer styling on comment, started reply on comments

--- Frontend/src/components/Comment.vue
+++ Frontend/src/components/Comment.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="comment">
+  <div class="comment" v-bind:class="{ 'child-comment': comment.parentCommentId !== null, 'parent-comment': comment.children.length !== 0 }">
     <p><a :href="`/user/${comment.user}`">{{ comment.user }}</a></p>
     <p>{{ comment.content }}</p>
+
+    <div class="links">
+      <a href="#" @click.prevent="createCommentBox">Reply</a>
+    </div>
 
     <div v-for="childComment in comment.children" v-bind:key="childComment.id">
       <Comment :comment="childComment" />
@@ -19,6 +23,12 @@ export default {
         return []
       }
     }
+  },
+  methods: {
+    createCommentBox() {
+      // TODO: this
+      console.log("wanna reply to this!")
+    }
   }
 }
 </script>
@@ -26,7 +36,25 @@ export default {
 <style scoped>
 .comment {
   padding: 16px;
+  padding-right: 0;
   margin: 8px;
   border: 1px solid black;
+}
+
+.parent-comment { }
+
+.child-comment {
+  margin-right: 0;
+  border-right: 0;
+}
+
+.links {
+  padding-right: 16px;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.links a {
+  margin-left: 16px;
 }
 </style>

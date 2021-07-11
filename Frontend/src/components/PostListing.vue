0b5d1e3be4bbc36513a5eb8750b9cbb35db5703a
Josh Perry <joshperry@outlook.com>
2021-07-11 01:23:07 +0100

    Styling on post listing

--- Frontend/src/components/PostListing.vue
+++ Frontend/src/components/PostListing.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="post">
-    <a :href="`/board/${post.board}/${post.url}`">{{ post.title }}</a>
-    
+    <a class="title" :href="`/board/${post.board}/${post.url}`">{{ post.title }}</a>
+
     <div class="details">
-      <p><a :href="`/user/${post.createdBy}`">{{ post.createdBy }}</a></p>
-      <p class="datetime" :title="post.createdDateTime">{{ humanDateTime }}</p>
+      <span>
+        Posted by <a :href="`/user/${post.createdBy}`">{{ post.createdBy }}</a> on <a :href="`/board/${post.board}`">{{ post.board }}</a>
+      </span>
+    </div>
+  
+    <div class="stats">
+      <p class="datetime" :title="post.createdDateTime"> {{ humanDateTime }}</p>
       <p>{{ post.commentCount }} comments</p>
     </div>
   </div>
@@ -38,23 +43,44 @@ export default {
 <style scoped>
 .details {
   display: flex;
+  font-size: 14px;
 }
 
-.details p {
-  flex: 1;
+p {
+  margin: 0;
+  padding: 0;
 }
 
 .details a {
   flex: 1;
 }
 
-.post {
-  padding: 16px;
-  margin: 8px;
-  border: 1px solid black;
+.details span a {
+  color: #F06543FF;
 }
 
-a {
+.stats {
+  display: flex;
+  font-size: 14px;
+}
+
+.stats p {
+  flex: 1;
+}
+
+.title {
+  font-size: 24px;
+}
+
+.post {
+  padding: 16px;
+  padding-right: 0;
+  margin: 8px;
+  border: 1px solid #CACACA;
+  border-left: 6px solid #F06543FF;
+}
+
+.title {
   text-decoration: none;
 }
 </style>
